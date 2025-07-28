51d3da86a27cbeef5efa8fab2550c52fb5be930c
FFFishes7 <es_fish@outlook.com>
2025-07-28 21:29:33 +0800

    1

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,3 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/animalface_coala.png");
   }
 };
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
-}
-
-myButton.onclick = function () {
-  setUserName();
-};
